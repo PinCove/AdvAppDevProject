a802f1eb7428f40e1fd6392aab9532e4a6046ac1
Stevo <stephen.spencer@okstate.edu>
2018-11-14 15:29:03 -0600

    YoloAmIRight?
    LOL

--- app/src/main/java/com/example/swspenc/advappdevproject/MainActivity.java
+++ app/src/main/java/com/example/swspenc/advappdevproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         //Delete Me
         Intent i = new Intent(this, MapsActivity.class);
         startActivity(i);
+
+        EditText input1 = (EditText) findViewById(R.id.editTextUName);
+        String UName = input1.getText().toString();
+        EditText input2 = (EditText) findViewById(R.id.editTextPassword);
+        String Password = input2.getText().toString();
 
         SqlStuff sql = new SqlStuff();
         sql.connectionclass();
